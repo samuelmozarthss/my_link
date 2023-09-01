e5cbdfa35dc062550f6f108fdeccb66b7d92e3e1
SamuelMozarth <contatosamuelm@gmail.com>
2023-08-31 23:54:45 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+import 'pages/index.dart';
+
+void main() {
+  runApp(const Index());
+}
---
+++ lib/pages/index.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class Index extends StatelessWidget {
+  const Index({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(color: Colors.black54),
+      child: Column(
+        children: [
+          Container(
+            height: 250,
+            decoration: BoxDecoration(color: Colors.black26),
+          )
+        ],
+      ),
+    );
+  }
+}
